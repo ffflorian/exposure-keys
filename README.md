afd3d100c09898a24e5c65597ad3749040a5070f
Florian Keller <github@floriankeller.de>
2020-07-01 14:33:23 +0200

    feat: Add riskCount

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # exposure-keys [![Build Status](https://github.com/ffflorian/exposure-keys/workflows/Build/badge.svg)](https://github.com/ffflorian/exposure-keys/actions/) [![npm version](https://img.shields.io/npm/v/exposure-keys.svg?style=flat)](https://www.npmjs.com/package/exposure-keys) [![Dependabot Status](https://api.dependabot.com/badges/status?host=github&repo=ffflorian/exposure-keys)](https://dependabot.com)
 
-A library to load and save Temporary Exposure Keys (TEK). For more information see https://www.google.com/covid19/exposurenotifications.
+A library to load and save Temporary Exposure Keys (TEK). For more information on the format see https://developers.google.com/android/exposure-notifications/exposure-key-file-format.
 
 ## Installation
 
--- src/ExposureKeys.ts
+++ src/ExposureKeys.ts
@@ -3,7 +3,8 @@ import {loadAsync} from 'jszip';
 
 function isZip(fileContent: Buffer): boolean {
   // eslint-disable-next-line no-magic-numbers
-  return new Buffer([0x50, 0x4b]).compare(fileContent.slice(0, 2)) === 0;
+  const zipHeader = [0x50, 0x4b];
+  return Buffer.from(zipHeader).compare(fileContent, 0, 2) === 0;
 }
 
 export async function loadZip(fileContent: Buffer): Promise<{keys: Buffer; signature: Buffer}> {
@@ -12,30 +13,45 @@ export async function loadZip(fileContent: Buffer): Promise<{keys: Buffer; signa
   }
 
   const unzippedData = await loadAsync(fileContent);
-  const keys = await unzippedData.file('export.bin')?.async('nodebuffer');
-  const signature = await unzippedData.file('export.sig')?.async('nodebuffer');
+  const keyData = await unzippedData.file('export.bin')?.async('nodebuffer');
+  const signatureData = await unzippedData.file('export.sig')?.async('nodebuffer');
 
-  if (!keys) {
-    throw new Error(`The zip file doesn't contain keys.`);
+  if (!keyData) {
+    throw new Error(`The zip file doesn't contain key data`);
   }
 
-  if (!signature) {
-    throw new Error(`The zip file doesn't contain a signature.`);
+  if (!signatureData) {
+    throw new Error(`The zip file doesn't contain a signature`);
   }
 
-  return {keys, signature};
+  return {keys: keyData, signature: signatureData};
 }
 
 export function loadKeys(fileContent: Buffer): TemporaryExposureKeyExport {
   if (isZip(fileContent)) {
-    throw new Error('You are trying to load a zip file. Please use loadZip() for that.');
+    throw new Error('You are trying to load a zip file. Please use `loadZip()` for that');
   }
 
-  // see https://developers.google.com/android/exposure-notifications/exposure-key-file-format
-  const bytesPadding = 16;
-  return TemporaryExposureKeyExport.decode(fileContent.slice(bytesPadding));
+  const wantedHeader = 'EK Export v1    ';
+  const header = fileContent.slice(0, wantedHeader.length);
+  const key = fileContent.slice(wantedHeader.length);
+
+  if (header.toString('utf-8') !== wantedHeader) {
+    throw new Error('The key is missing a correct header');
+  }
+
+  return TemporaryExposureKeyExport.decode(key);
 }
 
 export function loadSignature(fileContent: Buffer): TEKSignatureList {
   return TEKSignatureList.decode(fileContent);
+}
+
+export function riskCount(
+  temporaryExposureKeyExport: TemporaryExposureKeyExport,
+  transmissionRiskLevel: number
+): number {
+  return temporaryExposureKeyExport.keys.filter(
+    key => key.transmissionRiskLevel && key.transmissionRiskLevel === transmissionRiskLevel
+  ).length;
 }
